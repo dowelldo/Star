0ed7f738e7ddb01ed37318afc3eccaf29bab4ccd
tech <tech@techs-iMac.local>
2019-04-24 15:58:42 -0400

    Added comments to initial commit

--- Star.java
+++ Star.java
@@ -1,14 +1,35 @@
+/**
+ * This is a simple exersize in Java to print out a star whose size
+ * is based on a command line argument.
+ *
+ * @author Owen Dowell
+ * @version 1.0
+ */
+
 public class Star {
+	/**
+	 * Main method that drives the application
+	 *
+	 * @param args holds the specified size value
+	 */
 	public static void main(String[] args) {
-		int a;
+		int size;
+		// if user fails to use any arguments, print usage
 		try {
-			a = Integer.parseInt(args[0]);
+			size = Integer.parseInt(args[0]);
 		}
 		catch (ArrayIndexOutOfBoundsException e) {
 			printUsage();
 		}
+		
 	}
 
+	/**
+	 * printUsage:
+	 *
+	 * prints simple directions for operating the application.
+	 * Only executes when user fails to provide input.
+	 */
 	public static void printUsage() {
 		System.out.println("How Star Works:");
 		System.out.println("Simply type in a number to specify how big a star you want to print!");
